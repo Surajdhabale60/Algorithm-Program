5053cc78ecb00ae5e85d2d6e67fb5e4929ec7cc9
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-22 22:43:53 +0530

    [Suraj]Add:Seraching Word Using Array

---
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmProgram;
+
+namespace DataStructureAlgorithm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Algorithm Program");
+
+            BinarySearch binarysearch = new BinarySearch();
+            binarysearch.ReadTextFile(@"C:\Users\suraj\source\repos\217 .Net\Algorithm-Program\AlgorithmProgram\AlgorithmProgram\BinarySearch.txt");
+            binarysearch.BinarySearchOperation("Something");
+        }
+    }
+}
