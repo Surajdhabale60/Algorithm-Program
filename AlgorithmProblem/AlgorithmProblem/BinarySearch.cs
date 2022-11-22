cd95358c8722f08a257caedab922b752b10df5cb
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-22 23:37:46 +0530

    [Suraj]Add:Checking Word

---
+++ AlgorithmProblem/AlgorithmProblem/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    internal class BinarySearch
+    {
+        string[] binaryArr;
+        public void ReadTextFile(string filePath)
+        {
+            string words = File.ReadAllText(filePath);
+            binaryArr = words.Split(",");
+        }
+        public void BinarySearchOperation(string input)
+        {
+            foreach (var word in binaryArr)
+            {
+                if (word.Equals(input))
+                {
+                    Console.WriteLine("Found the word in the list using binary serach");
+                    return;
+                }
+            }
+            Console.WriteLine("Search elementt is not available in list of words");
+        }
+    }
+}
---
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgorithmProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while(flag)
+            {                
+                Console.WriteLine("select option\n 1.Binary Search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        BinarySearch binarySearch = new BinarySearch();
+                        binarySearch.ReadTextFile(@"C:\Users\suraj\source\repos\217 .Net\Algorithm-Program\AlgorithmProblem\AlgorithmProblem\TextFile1.txt");
+                        binarySearch.BinarySearchOperation("Something");
+                        break;
+                }
+            }
+        }
+    }
+}
