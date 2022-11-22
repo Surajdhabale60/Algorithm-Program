f1245d42a62d3cd46c37e782755781d9b79f1542
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-22 23:59:58 +0530

    [Suraj]Add:Swaping In Sequence

--- AlgorithmProblem/AlgorithmProblem/Program.cs
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while(flag)
             {                
-                Console.WriteLine("select option\n 1.Binary Search\n 2.Insertion Sort");
+                Console.WriteLine("select option\n 1.Binary Search\n 2.Insertion Sort\n 3.Bubble Sort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -16,11 +16,17 @@
                         binarySearch.ReadTextFile(@"C:\Users\suraj\source\repos\217 .Net\Algorithm-Program\AlgorithmProblem\AlgorithmProblem\TextFile1.txt");
                         binarySearch.BinarySearchOperation("Something");
                         break;
-                     case 2:
+                    case 2:
                         InsertionSort sort = new InsertionSort();
-                        int[] arr = { 5, 3, 1, 2, 4 };
-                        sort.Sort(arr);
+                        int[] binaryarr = { 5, 3, 1, 2, 4 };
+                        sort.Sort(binaryarr);
                         break;
+                    case 3:
+                        BubbleSort bubbleSort = new BubbleSort();                       
+                        int[] arr = { 8, 5, 7, 9, 6 };
+                        bubbleSort.Sort(arr);
+                        break;
+
 
                 }
             }
