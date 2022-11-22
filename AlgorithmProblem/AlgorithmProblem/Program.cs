28709c9f5cef856b31c7dab7c1452c4b4cfa4420
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-22 23:43:22 +0530

    [Suraj]Add:Swap Until getting In Assending Order

--- AlgorithmProblem/AlgorithmProblem/Program.cs
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while(flag)
             {                
-                Console.WriteLine("select option\n 1.Binary Search");
+                Console.WriteLine("select option\n 1.Binary Search\n 2.Insertion Sort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -16,6 +16,12 @@
                         binarySearch.ReadTextFile(@"C:\Users\suraj\source\repos\217 .Net\Algorithm-Program\AlgorithmProblem\AlgorithmProblem\TextFile1.txt");
                         binarySearch.BinarySearchOperation("Something");
                         break;
+                     case 2:
+                        InsertionSort sort = new InsertionSort();
+                        int[] arr = { 5, 3, 1, 2, 4 };
+                        sort.Sort(arr);
+                        break;
+
                 }
             }
         }
